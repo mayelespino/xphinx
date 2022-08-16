f92acc64784bd1baf525079de20c21c98eda9eba
mayel.espino <mayel.espino@gmail.com>
2022-08-16 19:31:32 +0000

    move from kube.

---
+++ xphinx_server/target/debug/deps/xphinx_server-a20c9711c578624f.d
@@ -0,0 +1,5 @@
+/home/mayel/GIT/xphinx/xphinx_server/target/debug/deps/xphinx_server-a20c9711c578624f: src/main.rs
+
+/home/mayel/GIT/xphinx/xphinx_server/target/debug/deps/xphinx_server-a20c9711c578624f.d: src/main.rs
+
+src/main.rs:
---
+++ xphinx_server/target/debug/xphinx_server.d
@@ -0,0 +1,1 @@
+/home/mayel/GIT/xphinx/xphinx_server/target/debug/xphinx_server: /home/mayel/GIT/xphinx/xphinx_server/src/main.rs
